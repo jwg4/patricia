c1dbefde45d1b74363aa76a7fd5673efdf5991a9
Jack <jack.grahl@yahoo.co.uk>
2016-09-08 00:11:06 +0100

    Correct name of function to be tested.

--- test_bitwise.py
+++ test_bitwise.py
@@ -5,6 +5,6 @@ class TestBitwise(unittest.TestCase):
     def test_lowest_one(self):
         lib = ctypes.cdll.LoadLibrary("./bitwise.so") 
         x = 104
-        res = lib.last_one(x)
+        res = lib.lowest_one(x)
         self.assertEqual(res, 8)
 
